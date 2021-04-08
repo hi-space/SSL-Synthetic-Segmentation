c70503a93e65a818c5a396fdf2f0553145f5fbfd
hi-space <hi-space.j@gmail.com>
2021-04-08 19:44:59 +0900

    gta datalodaer

--- core/data/dataloader/gta.py
+++ core/data/dataloader/gta.py
@@ -1,6 +1,7 @@
 """Prepare Cityscapes dataset"""
 import os
 import torch
+import scipy.io as io
 import numpy as np
 import logging
 
@@ -9,19 +10,6 @@ from .seg_data_base import SegmentationDataset
 
 
 class GTASegmentation(SegmentationDataset):
-    """GTA Semantic Segmentation Dataset.
-
-    Parameters
-    ----------
-    root : string
-        Path to Cityscapes folder. Default is './datasets/cityscapes'
-    split: string
-        'train', 'val' or 'test'
-    transform : callable, optional
-        A function that transforms the image
-    Examples
-    --------
-    """
     BASE_DIR = 'gta'
     NUM_CLASS = 19
 
@@ -46,12 +34,18 @@ class GTASegmentation(SegmentationDataset):
         [0, 0, 230],
         [119, 11, 32],
     ]
+    
+    void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, 34, -1]
+    valid_classes = [7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33,]
+    ignore_index = 250
+    class_map = dict(zip(valid_classes, range(19)))
 
     label_colours = dict(zip(range(NUM_CLASS), colors))
 
     def __init__(self, root='G:/data', split='train', mode=None, transform=None, **kwargs):
         super(GTASegmentation, self).__init__(root, split, mode, transform, **kwargs)
         self.root = os.path.join(root, self.BASE_DIR)
+        print(self.root)
         assert os.path.exists(self.root), "Please put dataset in {SEG_ROOT}/datasets/gta"
         self.images, self.mask_paths = _get_gta_pairs(self.root, self.split)
         assert (len(self.images) == len(self.mask_paths))
@@ -82,6 +76,8 @@ class GTASegmentation(SegmentationDataset):
                 img = self.transform(img)
             return img, os.path.basename(self.images[index])
         mask = Image.open(self.mask_paths[index])
+        maks = self._encode_segmap(np.array(mask, dtype=np.uint8))
+
         # synchrosized transform
         if self.mode == 'train':
             img, mask = self._sync_transform(img, mask)
@@ -98,6 +94,13 @@ class GTASegmentation(SegmentationDataset):
     def _mask_transform(self, mask):
         target = self._class_to_index(np.array(mask).astype('int32'))
         return torch.LongTensor(np.array(target).astype('int32'))
+    
+    def _encode_segmap(self, lbl):
+        for _i in self.void_classes:
+            lbl[lbl == _i] = self.ignore_index
+        for _i in self.valid_classes:
+            lbl[lbl == _i] = self.class_map[_i]
+        return lbl
 
     def __len__(self):
         return len(self.images)
@@ -123,8 +126,7 @@ def _get_gta_pairs(folder, split='train'):
                 if filename.endswith('.png'):
                     imgpath = os.path.join(root, filename)
                     foldername = os.path.basename(os.path.dirname(imgpath))
-                    maskname = filename
-                    maskpath = os.path.join(mask_folder, foldername, maskname)
+                    maskpath = os.path.join(mask_folder, '', filename)
                     if os.path.isfile(imgpath) and os.path.isfile(maskpath):
                         img_paths.append(imgpath)
                         mask_paths.append(maskpath)
@@ -133,23 +135,23 @@ def _get_gta_pairs(folder, split='train'):
         logging.info('Found {} images in the folder {}'.format(len(img_paths), img_folder))
         return img_paths, mask_paths
 
-    if split in ('train', 'val'):
-        img_folder = os.path.join(folder, 'leftImg8bit/' + split)
-        mask_folder = os.path.join(folder, 'gtFine/' + split)
-        img_paths, mask_paths = get_path_pairs(img_folder, mask_folder)
-        return img_paths, mask_paths
+    splits = io.loadmat(os.path.join(folder, 'split.mat'))
+    img_folder = os.path.join(folder, 'images')
+    label_folder = os.path.join(folder, 'labels')
+    
+    if split == 'train':
+        ids = splits['trainIds'][:,0]
+        
+    elif split == 'val':
+        ids = splits['valIds'][:200,0]
+    elif split == 'test':
+        ids = splits['testIds'][:,0]
     else:
         assert split == 'trainval'
-        logging.info('trainval set')
-        train_img_folder = os.path.join(folder, 'leftImg8bit/train')
-        train_mask_folder = os.path.join(folder, 'gtFine/train')
-        val_img_folder = os.path.join(folder, 'leftImg8bit/val')
-        val_mask_folder = os.path.join(folder, 'gtFine/val')
-        train_img_paths, train_mask_paths = get_path_pairs(train_img_folder, train_mask_folder)
-        val_img_paths, val_mask_paths = get_path_pairs(val_img_folder, val_mask_folder)
-        img_paths = train_img_paths + val_img_paths
-        mask_paths = train_mask_paths + val_mask_paths
-    return img_paths, mask_paths
+        ids = np.concatenate((splits['trainIds'][:,0], splits['valIds'][:,0]))
+    
+    image_paths, mask_paths = get_path_pairs(img_folder, label_folder)
+    return image_paths, mask_paths
 
 
 if __name__ == '__main__':
