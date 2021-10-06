d8e87c7b1012cac13474c52ca4ece631a62997da
hi-space <hispace.j@gmail.com>
2021-10-06 21:24:18 +0900

    test single

---
+++ Seg-Uncertainty/test-single.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python eval_single.py \
+   --restore-from ./snapshots/aagc_640x360_b4_single_origin/GTA5_30000.pth
+#    --restore-from ./snapshots/cityscapes_seg_d1000/GTA5_20000.pth
+    # --restore-from ./snapshots/aagc_640x360_b2_d1000/GTA5_20000.pth
