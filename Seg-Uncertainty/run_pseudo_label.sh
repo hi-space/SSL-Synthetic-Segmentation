7acaa3d5fd228d0b99cf32fb788c241323caf704
hi-space <hispace.j@gmail.com>
2021-10-08 16:01:06 +0900

    train label path

--- Seg-Uncertainty/run_pseudo_label.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# python generate_plabel_cityscapes.py --restore-from ./snapshots/GTA_TO_CITY_CO/GTA5_204000.pth
-python generate_plabel_cityscapes.py --restore-from ./snapshots/aagc_640x360_b1/GTA5_40000.pth
-
-# --restore-from ./snapshots/GTA_TO_CITY/GTA5_200000.pth
-
